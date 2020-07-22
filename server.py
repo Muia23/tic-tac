dc0bdaae9c6fb9c71f66e501af3f4241b9077f9e
Muia23 <wilfred.muema@gmail.com>
2020-07-22 07:27:20 +0300

    move the scocket commands to separate threads

--- server.py
+++ server.py
@@ -4,19 +4,37 @@ from grid import Grid
 surface = pygame.display.set_mode((600,600))
 pygame.display.set_caption('Tic-tac-toe')
 
+import threading
+
+def create_thread(target):
+    thread = threading.Thread(target=target)
+    thread.daemon = True
+    thread.start()
+
 
 import socket
 
 HOST = '127.0.0.1'
 PORT = 5555
 connection_established = False
+conn , addr = None, None
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.bind((HOST,PORT))
 sock.listen(1)
 
-conn, addr= sock.accept()
 
+def receive_data():
+    pass
+
+def waiting_for_connection():
+    global connection_established, conn, addr
+    conn, addr= sock.accept()
+    print('client is connected')
+    connection_established = True
+    receive_data()
+
+create_thread(waiting_for_connection)
 
 grid = Grid()
 
